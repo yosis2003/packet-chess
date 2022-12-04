fe698d1a5c33a3c17ddbba050c435c83b839a517
admin <admin@BCS-Ubuntu-35.lrccd.ad.losrios.edu>
2022-12-03 18:31:22 -0800

    main changes

--- code/main.cpp
+++ code/main.cpp
@@ -1,3 +1,19 @@
 // Packet Chess Game
 // Creators: Matthew Craig, Yousif Alnomani
 // CISP 400
+
+
+#include <iostream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+using namespace sf;
+using namespace std;
+
+int main(){
+
+    VideoMode vm (1920, 1080);
+    RenderWindow(vm, "Packet Chess", Style::Default);
+    
+
+}
